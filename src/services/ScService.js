03d348fdb1589a595788dfcea02a3a0b3682d0ef
alexdotwells <ajwells99@gmail.com>
2018-01-24 21:55:50 -0600

    initial commit (refactored sc service)

---
+++ src/services/ScService.js
@@ -0,0 +1,23 @@
+import SC from 'soundcloud';
+
+export default class ScService {
+    
+    static async getFavorites() {
+        var userId = 63317612;
+
+        // Initialize SoundCloud Client
+        SC.initialize({
+            client_id: 'aa43f640526cb3f753a3a2ce40a340b4',
+            redirect_uri: 'http://localhost:3000/callback'
+        });  
+
+        // Get User Favorites from SC API
+        const res = await SC.get(`/users/${userId}/favorites`,{
+            limit: 20,
+            linked_partitioning: 1        
+        })
+
+        return res.collection;
+    }
+
+}
