8966d7bc64b8277b94741fc98fcfc8145d8efea8
lofisoul <jason.nolfi@gmail.com>
2018-01-28 21:13:02 -0600

    Update to master

--- src/components/App.js
+++ src/components/App.js
@@ -10,20 +10,14 @@ export default class App extends Component {
         this.state = {
             title: 'React SoundCloud Client'
         };
-        // this.getTracks = this.getTracks.bind(this);
-        // this.scInit = this.scInit.bind(this);
     }
-
-    // componentDidMount() {
-    //       this.getTracks();
-    // }
 
     render() {
         return (
             <div>
                 <Header title={this.state.title} />
                 <div className="mt-5">
-                    <Favorites details={this.state.tracks} />
+                    <Tracks />
                 </div>
             </div>
         );
--- src/services/SoundCloudService.js
+++ src/services/SoundCloudService.js
@@ -1,20 +1,20 @@
 import SC from 'soundcloud';
 
 export default class ScService {
-    
+
     static async getFavorites() {
         var userId = 63317612;
 
         // Initialize SoundCloud Client
         SC.initialize({
-            client_id: '##CLIENT_ID##',
+            client_id: 'aa43f640526cb3f753a3a2ce40a340b4ç',
             redirect_uri: 'http://localhost:3000/callback'
-        });  
+        });
 
         // Get User Favorites from SC API
         const res = await SC.get(`/users/${userId}/favorites`,{
             limit: 20,
-            linked_partitioning: 1        
+            linked_partitioning: 1
         })
 
         return res.collection;
