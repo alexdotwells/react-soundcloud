ed3e221e0afb893348eb903c544de2cfba6db614
alexdotwells <ajwells99@gmail.com>
2018-01-28 18:27:37 -0600

    added loop to modify default artwork size

--- src/services/SoundCloudService.js
+++ src/services/SoundCloudService.js
@@ -3,19 +3,25 @@ import SC from 'soundcloud';
 export default class ScService {
     
     static async getFavorites() {
-        var userId = 63317612;
+        let userId = 63317612;
 
         // Initialize SoundCloud Client
         SC.initialize({
-            client_id: '##CLIENT_ID##',
+            client_id: 'aa43f640526cb3f753a3a2ce40a340b4',
             redirect_uri: 'http://localhost:3000/callback'
         });  
 
         // Get User Favorites from SC API
-        const res = await SC.get(`/users/${userId}/favorites`,{
-            limit: 20,
+        let res = await SC.get(`/users/${userId}/favorites`,{
+            limit: 300,
             linked_partitioning: 1        
         })
+
+        // Modify artwork image size
+        res.collection.forEach(function(track,index){
+            if (track.artwork_url != null)
+                track.artwork_url = track.artwork_url.replace("-large","-t300x300");
+        });
 
         return res.collection;
     }
